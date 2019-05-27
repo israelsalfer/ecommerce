a3b3301bcd58a9ee35af1a71474400bacd2fcd8f
israelsalfer <israelsalfer@gmail.com>
2019-05-27 14:20:21 -0300

    criamos pageadmin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/pageAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Hcode;
+
+class pageAdmin extends page {
+
+    public function __construct($opts = array(),$tpl_dir = "/views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+
+
+
+
+
+}
+
+
+
+
+
+?>
