1fb94e9f6e5cb11c356d55af3bf7863301dd42b3
israelsalfer <israelsalfer@gmail.com>
2019-06-07 16:18:51 -0300

    configuracao de login pronto

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -20,6 +20,8 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
@@ -38,12 +40,21 @@ $app->get('/admin/login', function() {
 
 });
 
-$app->get('/admin/login', function() {
+$app->post('/admin/login', function() {
     
-	user::login($_POST["login"], $_POST["password"]);
+	User::login($_POST["login"], $_POST["password"]);
 
 	header("Location: /admin");
 	exit;
+
+});
+
+$app -> get("/admin/logout",function(){
+
+User::logout();
+
+header("Location: /admin/login");
+exit;
 
 });
 
